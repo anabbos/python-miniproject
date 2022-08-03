a4ea334ba1f2c9f7ca1567306d2e815975f40a74
theabbos <abbosdevv@gmail.com>
2022-08-03 09:57:28 +0500

    the end

--- colorchooser.py
+++ colorchooser.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import colorchooser
 
+def click():
+    color = colorchooser.askcolor()
+    print(color)
+    colorHex = color[1]
+    print(colorHex)
+    window.config(bg=colorHex)
 
 window = Tk()
 window.geometry("420x420")
