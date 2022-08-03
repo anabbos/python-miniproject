b78df1b7941d6e4d25f2ed065eb826bf37c6e65a
theabbos <abbosdevv@gmail.com>
2022-08-03 09:56:16 +0500

    created window | tkinter with button

--- colorchooser.py
+++ colorchooser.py
@@ -1,2 +1,16 @@
 from tkinter import *
 from tkinter import colorchooser
+
+window = Tk()
+window.geometry("420x420")
+button = Button(text="Click",
+                padx=60,
+                pady=30,
+                font=("Arial",20),
+                bg="red",
+                fg="white",
+                relief="ridge",
+                command="click")
+button.place(x=120,y=140)
+
+window.mainloop()
